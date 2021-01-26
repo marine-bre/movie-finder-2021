caece59119c95b19cca15a216dfed9268ef78459
marine-bre <marine_bretonniere@hotmail.co.uk>
2021-01-26 16:28:18 +0200

    fix

--- client/src/contexts/socket.js
+++ client/src/contexts/socket.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import io from 'socket.io-client'
 
-const ENDPOINT = 'https://movieees.herokuapp.com/'
-export const socket = io(ENDPOINT);
+// const ENDPOINT = 'https://movieees.herokuapp.com/'
+// export const socket = io(ENDPOINT);
 
 // export let socket = io.connect('http://localhost:5000/');
 
 
-// export let socket = io.connect(window.location.hostname)
+export let socket = io.connect(window.location.hostname)
 
 export const SocketListen = ({preferences, setPreferences, setReceived}) => {
 
